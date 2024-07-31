786e5c37f828a3533a1b08746baf7ac9e09614e0
unknown <gabriel.br1994@gmail.comm>
2024-07-30 20:21:34 -0400

    envio do projeto

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,18 @@
+import styles from "./Home.module.css"
+import savings from "../../img/savings.svg"
+import LinkButton from "../layout/LinkButton"
+function Home(){
+    const port = import.meta.env.VITE_PORT;
+    console.log(port)
+    return (
+    <section className={styles.home_container}>
+        <h1>Bem-vindo ao <span>Costs</span></h1>
+        <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+        <LinkButton to="/newProject" text="Criar Projeto"/>
+
+        <img src={savings} alt="Costs" />
+    </section>
+    )
+}
+
+export default Home
---
+++ src/components/pages/NewProject.jsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
+
+function NewProject() {
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    const port = import.meta.env.VITE_PORT;
+    //initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch(`${port}/projects`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+        //redirect
+        const state = { message: "Projeto criado com sucesso!" };
+        navigate("/projects", {state});
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+    </div>
+  );
+}
+
+export default NewProject;
